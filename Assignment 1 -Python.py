4899d6abc380783dcd4d858adb7615ee07a183f6
iPushpeshPandey <90572302+iPushpeshPandey@users.noreply.github.com>
2022-07-23 23:21:40 +0530

    Add files via upload

---
+++ Assignment 1 -Python.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Assignment 1
+
+# Question no 1
+
+# * is mathematical operator.
+# "hello" is a value.
+# -87.8 is a value.
+# - is a mathematical operator.
+# / is a mathematical operator.
+# + is a mathematical operator.
+# 6 is a value.
+# 
+# 
+
+# Question 2
+# 
+
+# Difference between a string or a variable is as follows
+# (1) A string is a mathematrical value whose value is fixed where as a variable is quantity that can 
+# assume any value.
+# (2) A string is a information that can be strored in any variable where as a variable is something that stores the informations.
+# (3) A string is a mathematical value represented in textual format where as a variable can represent any data types.
+# 
+# 
+
+# Question 3
+
+# In[ ]:
+
+
+The three different data types are:
+    Integers: These are the datatype representing integers as values.
+        example: 1,2
+    string : These are the datatypes representing text.
+        example: 'pushpesh'
+    float: These are the datatypes representing the decimal numbers.
+        example: 12.1
+
+
+# Question 4
+
+# In[ ]:
+
+
+An Expression is a combinations of the mathematical values , operators to produce some other values.
+All the expressions store the values or the informations.
+example: a string is a expresssion that stores the value of the string.
+
+
+# Question 5
+
+# spam=10
+# is an expression, as it stores the value of spam.
+# where as a statement only displays a value its cannot be reduced to a value, it only do the things whivh an expression says.
+# 
+
+# Question 6
+
+# In[7]:
+
+
+bacon=22
+bacon+1
+
+
+# Question 7
+
+# In[5]:
+
+
+'spam'+'spamspam'
+
+
+# In[6]:
+
+
+'spam'*3
+
+
+# Question 8
+
+# In[ ]:
+
+
+egg can be a valid name as it does not represnts a value its a statement that can assign any value.
+where as 100 is a mathematical value it cannot store any value.
+
+
+# Question 9
+
+# In[ ]:
+
+
+for integers we can take an use the function int.
+for floating point numbers we can use float.
+for string we can use the str function.
+
+
+# Question 10
+# 
+The Error here is when we are adding the integer with the string.
+so to fix the problem we need to convert the integer data type  to the the string data type
+
+# In[3]:
+
+
+"i have eaten"+" 99 "+"burritos"
+
